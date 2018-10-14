35d71a6bbae38abbb8efd0f6ca02306527ac3a06
Wahid <wahid@Wahids-MacBook.local>
2018-10-14 16:28:34 -0700

    create the project

---
+++ StepLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StepLogger
+//
+//  Created by Wahid on 10/14/18.
+//  Copyright © 2018 Wahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ StepLoggerTests/StepLoggerTests.swift
@@ -0,0 +1,34 @@
+//
+//  StepLoggerTests.swift
+//  StepLoggerTests
+//
+//  Created by Wahid on 10/14/18.
+//  Copyright © 2018 Wahid. All rights reserved.
+//
+
+import XCTest
+@testable import StepLogger
+
+class StepLoggerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
